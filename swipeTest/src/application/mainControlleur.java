c347db02ce401c5be970bfce011a89239bc9ccb7
gerome12 <gerome.pasquier@gmail.com>
2017-10-31 09:06:11 +0100

    add touch event

--- swipeTest/src/application/mainControlleur.java
+++ swipeTest/src/application/mainControlleur.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.scene.input.SwipeEvent;
+import javafx.scene.input.TouchEvent;
 import javafx.scene.shape.Rectangle;
 
 public class mainControlleur {
@@ -15,10 +16,16 @@ public class mainControlleur {
 		rect.setOnSwipeLeft(this::sw);
 		rect.setOnSwipeRight(this::sw);
 		rect.setOnSwipeUp(this::sw);
+		rect.setOnTouchPressed(this::touch);
 	}
 	
 	private void sw(SwipeEvent event)
 	{
 		System.out.println(event.getX()+"_"+event.getY());
+	}
+	
+	private void touch(TouchEvent event)
+	{
+		System.out.println(event.getTouchPoint());
 	}
 }
