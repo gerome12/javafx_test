ac8e5877ef1952dd7d3300f0d8eef60ac20ace94
gerome12 <gerome.pasquier@gmail.com>
2017-11-06 15:51:10 +0100

    animation zoom

--- weasis_tactile_canvas/src/application/MainCanvas.java
+++ weasis_tactile_canvas/src/application/MainCanvas.java
@@ -2,28 +2,37 @@ package application;
 
 import java.util.concurrent.FutureTask;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.ParallelTransition;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class MainCanvas extends Canvas{
 	
 	private Image image;
 	public String imageName;
 	
-	private double zoomFactor=1;
+	private DoubleProperty zoomFactorProperty = new SimpleDoubleProperty(1);
 	private double translateX=0;
 	private double translateY=0;
 	private double scroll=0;
 	private double contrasteX=1;
 	private double contrasteY=1;
 	
+	private ParallelTransition parallelTransition;
+	
 	private ScrollController sc;
 	
 	public MainCanvas() {
-		image =null;
+
 		this.widthProperty().addListener(new ChangeListener<Number>() {
 		    @Override
 		    public void changed(ObservableValue<? extends Number> observable,
@@ -38,25 +47,22 @@ public class MainCanvas extends Canvas{
 		    	draw();
 		    }
 		});
+		initTimeLine();
 	}
 		
-	public MainCanvas(String image) {
-		this.imageName = image;
-		this.image = new Image(image);
-		this.widthProperty().addListener(new ChangeListener<Number>() {
-		    @Override
-		    public void changed(ObservableValue<? extends Number> observable,
-		            Number oldValue, Number newValue) {
-		    	draw();
-		    }
-		});
-		this.heightProperty().addListener(new ChangeListener<Number>() {
-		    @Override
-		    public void changed(ObservableValue<? extends Number> observable,
-		            Number oldValue, Number newValue) {
-		    	draw();
-		    }
-		});
+	
+	private void initTimeLine() {
+		parallelTransition = new ParallelTransition();
+		
+		Timeline timeline;
+		KeyValue kv;
+		KeyFrame kf;
+    	timeline = new Timeline();
+    	kv = new KeyValue(zoomFactorProperty, 1);
+    	kf = new KeyFrame(Duration.millis(1000), kv);
+    	timeline.getKeyFrames().add(kf);
+		
+        parallelTransition.getChildren().addAll(timeline);
 	}
 	
 	public void setScrollBar(ScrollController sc) {
@@ -72,16 +78,16 @@ public class MainCanvas extends Canvas{
 		this.getGraphicsContext2D().clearRect(0, 0, this.getWidth(), this.getHeight());
 		this.getGraphicsContext2D().setGlobalAlpha(contrasteX);
 		
-		this.getGraphicsContext2D().drawImage(image, translateX, translateY, this.getWidth()*zoomFactor,this.getHeight()*zoomFactor);
+		this.getGraphicsContext2D().drawImage(image, translateX, translateY, this.getWidth()*this.zoomFactorProperty.get(),this.getHeight()*this.zoomFactorProperty.get());
 		this.getGraphicsContext2D().setStroke(Color.RED);
 		this.getGraphicsContext2D().setLineWidth(scroll);
 		if(scroll!=0)
-			this.getGraphicsContext2D().strokeRect(translateX+scroll/2, translateY+scroll/2, this.getWidth()*zoomFactor-scroll,this.getHeight()*zoomFactor-scroll);
+			this.getGraphicsContext2D().strokeRect(translateX+scroll/2, translateY+scroll/2, this.getWidth()*this.zoomFactorProperty.get()-scroll,this.getHeight()*this.zoomFactorProperty.get()-scroll);
 	}
 	
 	public void zoom(double zoomFactor,double x,double y) {
-		if(this.zoomFactor >= 0.75 ) {
-			this.zoomFactor *=zoomFactor;
+		if(this.zoomFactorProperty.get() >= 0.75 ) {
+			this.zoomFactorProperty.set(zoomFactor * this.zoomFactorProperty.get());
 			this.translateX += ((translateX - x)*zoomFactor) - (translateX - x);
 			this.translateY += ((translateY - y)*zoomFactor) - (translateY - y); 
 		}
@@ -89,8 +95,8 @@ public class MainCanvas extends Canvas{
 	}
 	
 	public void zoomEnd() {
-		if(this.zoomFactor < 1) {
-			this.zoomFactor =1;
+		if(this.zoomFactorProperty.get() < 1) {
+			parallelTransition.playFromStart();
 			this.translateX = 0;
 			this.translateY = 0; 
 		}
@@ -117,7 +123,7 @@ public class MainCanvas extends Canvas{
 	}
 	
 	public void reset() {
-		zoomFactor=1;
+		this.zoomFactorProperty.set(1);
 		translateX=0;
 		translateY=0;
 		scroll=0;
