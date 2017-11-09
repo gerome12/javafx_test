4eb857c01b12d07df23f6ac6a000a11f641a1175
gerome12 <gerome.pasquier@gmail.com>
2017-11-09 17:48:11 +0100

    amélioration mineur

--- weasis_tactile_canvas/src/application/PatientController.java
+++ weasis_tactile_canvas/src/application/PatientController.java
@@ -1,6 +1,9 @@
 package application;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
@@ -9,6 +12,8 @@ public class PatientController {
 	@FXML
 	TitledPane patient;
 	@FXML
+	Group patientGroup;
+	@FXML
 	VBox patientImageVBox;
 	
 	private String patientName;
@@ -16,6 +21,23 @@ public class PatientController {
 	@FXML
 	public void initialize() {
 		patientImageVBox.prefWidthProperty().bind(patient.prefWidthProperty());
+		
+		
+		patient.expandedProperty().addListener(new ChangeListener<Object>() {
+        	@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
+        		if((Boolean)newVal == true)
+        		{
+	        		VBox vb = (VBox)patientGroup.getParent();
+	        		
+	        		for (Object iterable_element : vb.getChildren()) {
+	        			Group g = (Group)iterable_element;
+	        			TitledPane tp = (TitledPane)g.getChildren().get(0);
+	        			if(tp.getId() != patient.getId())
+	        				tp.setExpanded(false);
+	        		}
+        		}
+            }
+        });
 	}
 	
 	public void addPeliculeCanvas(PeliculeCanvas peliculeCanvas) {
@@ -30,5 +52,14 @@ public class PatientController {
 	public void SetExpanded(Boolean value) {
 		patient.setExpanded(value);
 	}
+	
+	public void Open() {
+		patient.setExpanded(true);
+	}
+	
+	public void Close() {
+		patient.setExpanded(false);
+	}
+	
 	
 }
